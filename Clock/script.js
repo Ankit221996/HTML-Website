6ecedc8c15b77c649fbd68092082e9f8e4f62db1
Ankit Negi <140000849+Ankit221996@users.noreply.github.com>
2025-03-25 13:20:22 +0530

    Add files via upload

---
+++ Clock/script.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    document.getElementById("Hour").style.transform = `rotate(${hrotation}deg)`;
+    document.getElementById("Minute").style.transform = `rotate(${mrotation}deg)`;
+    document.getElementById("Second").style.transform = `rotate(${srotation}deg)`;
+},1000);
